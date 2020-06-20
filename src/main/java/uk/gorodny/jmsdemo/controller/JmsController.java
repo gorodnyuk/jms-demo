4b6c2037f0beea1e5e93464d66d8308ba889110f
Sergey Gorodnyuk <sergeygorodnyuk@yandex.ru>
2020-06-20 13:11:38 +0300

    small refactor

--- src/main/java/uk/gorodny/jmsdemo/controller/JmsController.java
+++ src/main/java/uk/gorodny/jmsdemo/controller/JmsController.java
@@ -1,8 +1,10 @@
 package uk.gorodny.jmsdemo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gorodny.jmsdemo.model.User;
@@ -11,15 +13,18 @@ import uk.gorodny.jmsdemo.model.User;
 @RequestMapping("/")
 public class JmsController {
 
+    @Value("${queue.name}")
+    private String queueName;
+
     @Autowired
     private JmsTemplate jmsTemplate;
 
-    @GetMapping("/produce")
-    public String produce() {
+    @GetMapping("/produce/{name}/{age}")
+    public String produce(@PathVariable("name") String name, @PathVariable("age") int age) {
         User user = new User();
-        user.setName("Mike");
-        user.setAge(30);
-        jmsTemplate.convertAndSend("queue2", user);
+        user.setName(name);
+        user.setAge(age);
+        jmsTemplate.convertAndSend(queueName, user);
         return "done";
     }
 }
